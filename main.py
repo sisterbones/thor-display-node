63ef551c3fe16e374dfa8ccfcd3e9a926fa9e6e3
Eoghan Ó Bogail <eoghan@gaeilgemail.com>
2024-12-26 22:20:13 +0000

    Crash handling

--- main.py
+++ main.py
@@ -41,6 +41,20 @@ logging.basicConfig(
 )
 log = logging.getLogger("rich")
 
+def get_ip():
+    # https://stackoverflow.com/a/28950776
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
 # ~~ GET IP ADDRESS AND HOSTNAME ~~
 # ip_address = IPv4Address(nmcli.connection.show(nmcli.connection()[0].name)['IP4.ADDRESS[1]'].split('/')[0])
 # hostname = gethostname()
